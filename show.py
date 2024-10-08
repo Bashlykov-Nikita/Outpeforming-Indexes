d36790ecd4086f0489712c6a8274ac3ff47d65fb
Bashlyklov Mykyta <bashlykovnikita@gmail.com>
2024-10-08 13:22:12 +0300

    Stats

--- show.py
+++ show.py
@@ -14,9 +14,21 @@ def show_index_historical_data(name):
 def show_stats(name):
     m_ind_data = pd.DataFrame(m.resample_data(m.fetch_index_data(name))["Return"])
     stats_df = c.summary_stats(m_ind_data)
-    st.write(stats_df)
+    st.write(
+        stats_df[
+            ["Annualized Return", "Annualized Vol", "Sharpe Ratio", "Max Drawdown"]
+        ]
+    )
+    st.write(
+        stats_df[
+            ["Skewness", "Kurtosis", "Cornish-Fisher VaR (5%)", "Historic CVaR (5%)"]
+        ]
+    )
 
 
 # @st.cache_data
 def growth_plot(name):
     st.line_chart(data=(1 + m.fetch_index_data(name, True)).cumprod(), y="Return")
+
+
+# TODO: One show function for index
