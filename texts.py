2c385fac611e71c5bf8351c4ab5033f99312d6ec
Bashlyklov Mykyta <bashlykovnikita@gmail.com>
2024-11-05 19:35:26 +0300

    About Portfolios 1

--- texts.py
+++ texts.py
@@ -6,10 +6,17 @@ def index_none_text():
 
 
 def portfolio_none_text(selected_portfolio):
-    portfolios_desc = """Maximum Sharpe Ratio - a portfolio that aims for the highest possible returns for a given level of risk.
-Global Minimum Variance - a portfolio with the smallest possible fluctuations in value.
-Equally Weighted - a portfolio where each stock has the same percentage.
-Cap Weighted - a portfolio where each stock's weight is based on its market value.
-Equal Risk Contribution - a portfolio where each stock contributes equally to the overall portfolio risk."""
     if selected_portfolio == "None":
-        st.markdown(portfolios_desc)
+        st.subheader("About Portfolios:")
+        st.write(
+            "#### - 📈Maximum Sharpe Ratio - a portfolio that aims for the highest possible returns for a given level of risk."
+        )
+        st.write(
+            "#### - 🛡️Global Minimum Variance - a portfolio with the smallest possible fluctuations in value."
+        )
+        st.write(
+            "#### - ⚖️Equally Weighted - a portfolio where each stock has the same percentage."
+        )
+        st.write(
+            "#### - 🧩Equal Risk Contribution - a portfolio where each stock contributes equally to the overall portfolio risk."
+        )
