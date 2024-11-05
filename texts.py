777a57d55583396be8162c893c4a944af0ad00e7
Bashlyklov Mykyta <bashlykovnikita@gmail.com>
2024-11-05 20:18:55 +0300

    Note 1

--- texts.py
+++ texts.py
@@ -11,6 +11,29 @@ def portfolio_none_text(selected_portfolio):
         st.write(
             "#### - 📈Maximum Sharpe Ratio - a portfolio that aims for the highest possible returns for a given level of risk."
         )
+        st.markdown(
+            """
+        <div class="note-block">
+            <strong>Note:</strong> This is an important note for the user.
+        </div>
+        """,
+            unsafe_allow_html=True,
+        )
+
+        st.markdown(
+            """
+        <style>
+            .note-block {
+                padding: 1rem;
+                border-radius: 0.5rem;
+                background-color: #062633;
+                border-left: 4px solid rgb(0 192 242 / 1);
+                margin-bottom: 1rem;
+            }
+        </style>
+        """,
+            unsafe_allow_html=True,
+        )
         st.write(
             "#### - 🛡️Global Minimum Variance - a portfolio with the smallest possible fluctuations in value."
         )
