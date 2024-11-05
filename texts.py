6b28d7936e48d897492a07a4edb42919231b16e6
Bashlyklov Mykyta <bashlykovnikita@gmail.com>
2024-11-05 20:51:46 +0300

    Note 2

--- texts.py
+++ texts.py
@@ -1,6 +1,36 @@
 import streamlit as st
 
 
+def note_text(note_massage: str):
+    st.markdown(
+        f"""
+    <div class="note-block">
+        <strong class="note-text">Note:</strong> {note_massage}.
+    </div>
+    """,
+        unsafe_allow_html=True,
+    )
+
+    st.markdown(
+        """
+    <style>
+        .note-block {
+            padding: 1rem;
+            border-radius: 0.5rem;
+            background-color: #062633;
+            border-left: 4px solid rgb(0 192 242 / 1);
+            margin-bottom: 1rem;
+        }
+        .note-text {
+            color: rgb(0 192 242 / 1)
+        }
+
+    </style>
+    """,
+        unsafe_allow_html=True,
+    )
+
+
 def index_none_text():
     return ""
 
@@ -9,37 +39,17 @@ def portfolio_none_text(selected_portfolio):
     if selected_portfolio == "None":
         st.subheader("About Portfolios:")
         st.write(
-            "#### - 📈Maximum Sharpe Ratio - a portfolio that aims for the highest possible returns for a given level of risk."
+            "##### - 📈Maximum Sharpe Ratio - a portfolio that aims for the highest possible returns for a given level of risk."
         )
-        st.markdown(
-            """
-        <div class="note-block">
-            <strong>Note:</strong> This is an important note for the user.
-        </div>
-        """,
-            unsafe_allow_html=True,
-        )
-
-        st.markdown(
-            """
-        <style>
-            .note-block {
-                padding: 1rem;
-                border-radius: 0.5rem;
-                background-color: #062633;
-                border-left: 4px solid rgb(0 192 242 / 1);
-                margin-bottom: 1rem;
-            }
-        </style>
-        """,
-            unsafe_allow_html=True,
+        note_text(
+            "MSR portfolio contains no exposure to unrewarded risk (contains only systematic risc)"
         )
         st.write(
-            "#### - 🛡️Global Minimum Variance - a portfolio with the smallest possible fluctuations in value."
+            "##### - 🛡️Global Minimum Variance - a portfolio with the smallest possible fluctuations in value."
         )
         st.write(
-            "#### - ⚖️Equally Weighted - a portfolio where each stock has the same percentage."
+            "##### - ⚖️Equally Weighted - a portfolio where each stock has the same percentage."
         )
         st.write(
-            "#### - 🧩Equal Risk Contribution - a portfolio where each stock contributes equally to the overall portfolio risk."
+            "##### - 🧩Equal Risk Contribution - a portfolio where each stock contributes equally to the overall portfolio risk."
         )
